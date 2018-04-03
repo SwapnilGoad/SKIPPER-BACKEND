14fbc8c1fb19b7434d327002b831310affdd9a0e
ajay <ajay@khusaki.com>
2018-04-03 20:42:04 +0530

    changes in

--- lambda/admin.js
+++ lambda/admin.js
@@ -9,38 +9,51 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient({convertEmptyValues: true});
 //method: get path: /admin/agency/{id}
 module.exports.login = (event, context, callback) => {
    //Get the details of a particular agency record code goes here 
- var body = JSON.parse(event.body);
-  
-var params = {
-    TableName: process.env.ADMIN_TABLE,
-    Key:{
-        "user_name": body.user_name
-        //"password": body.password
-    }
-};
-
-dynamoDb.get(params, function(error, data) {
-    if (error) {
-      callback(error);
-    }else{
-        if(Object.keys(data).length == 0){
-            //callback(error,{'message':'Incorrect user name or password ','success':'4'});
-            callback(null, successResponseBuilder(JSON.stringify({'message':'Incorrect user name or password ','success':'4'})));
-        }else{
-            var password= data.Item.password;
-            if(password == body.password){
-                callback(null, successResponseBuilder(JSON.stringify({'user':{'user_name': data.Item.user_name, 'password': data.Item.password,'status':data.Item.status},'success':'1'})));
-            }else{
-               //callback(error,{'message':'Incorrect user name or password ','success':'4'}); 
-               callback(null, successResponseBuilder(JSON.stringify({'message':'Incorrect user name or password ','success':'4'})));
+    try{
+        var body = JSON.parse(event.body);
+        
+        var params = {
+            TableName: process.env.ADMIN_TABLE,
+            Key:{
+                "user_name": body.user_name
+                //"password": body.password
             }
-           
-        }
+        };
+
+        dynamoDb.get(params, function(error, data) {
+            if (error) {
+            callback(error);
+            }else{
+                if(Object.keys(data).length == 0){
+                    //callback(error,{'message':'Incorrect user name or password ','success':'4'});
+                    callback(null, successResponseBuilder(JSON.stringify({'message':'Incorrect user name or password ','success':'4'})));
+                }else{
+                    var password= data.Item.password;
+                    if(password == body.password){
+                        callback(null, successResponseBuilder(JSON.stringify({'user':{'user_name': data.Item.user_name, 'password': data.Item.password,'status':data.Item.status},'success':'1'})));
+                    }else{
+                    //callback(error,{'message':'Incorrect user name or password ','success':'4'}); 
+                    callback(null, successResponseBuilder(JSON.stringify({'message':'Incorrect user name or password ','success':'4'})));
+                    }
+                
+                }
+            }
+            // callback(error, data);
+        
+        
+        });
+
+    }catch(error)
+    {
+        callback(null, failureResponseBuilder(
+            409,
+            JSON.stringify({
+                message: `Internal Server Error`,
+                event: event,
+                error: error
+            })
+        ))
     }
-    // callback(error, data);
-  
-   
-});
 
 };
 
